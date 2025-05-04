4a038b905f66f7deba9df3acb45810fc9be4261a
Pastel Dastel <pastel.dastel@gmail.com>
2025-05-04 22:28:35 +0200

    Messo tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 import { RouteReuseStrategy } from '@angular/router';
 import { routes } from './app/app.routes';
-import './tailwind-output.css';
+import './styles.css';
+import './tailwind.css';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),                         // ✅ Aggiungi questo se manca
+    provideHttpClient(), // ✅ Aggiungi questo se manca
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideIonicAngular(),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
